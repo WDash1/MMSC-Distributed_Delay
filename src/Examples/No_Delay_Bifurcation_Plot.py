c020a9f7c47e3ae4e2c00c0c612c431098add7ee
William <William@ITSs-iMac.local>
2020-06-27 15:25:03 +0100

    This update modifies the colour scheme used and correctly orientates the axes for the no delay bifurcation plot.

--- src/Examples/No_Delay_Bifurcation_Plot.py
+++ src/Examples/No_Delay_Bifurcation_Plot.py
@@ -33,9 +33,9 @@ def computeEigenvalues(a,b):
 
 # Generate a list of a and b values that will be used to produce a bifurcation
 # plot.    
-a_amt = 50;
+a_amt = 500;
 a_values = NP.linspace(0.001, 3, num=a_amt);
-b_amt=50;
+b_amt=500;
 b_values = NP.linspace(0.001, 3, num=b_amt);
 
 
@@ -46,11 +46,11 @@ fixed_point_types = NP.zeros((a_amt,b_amt));
 
 
 # The numeric values that will be assigned for each type of fixed point.
-source_colour_value = 5;
-sink_colour_value = 3;
-saddle_colour_value = 10;
-unstable_spiral_colour_value = -5;
-stable_spiral_colour_value = -3;
+source_colour_value = 6;
+sink_colour_value = 0;
+saddle_colour_value = 2;
+unstable_spiral_colour_value = 8;
+stable_spiral_colour_value = 10;
 
 
 # Iterate through each combination of a and b values in the a_values and
@@ -66,33 +66,40 @@ for i in range(0, a_amt):
         real2 = NP.real(eigenvalues[1]);
         imag1 = NP.imag(eigenvalues[0]);
         imag2 = NP.imag(eigenvalues[1]);
+      
+        #fixed_point_types[i][j] = max([real1, real2]);
         
         # If the fixed point is asymptotically unstable.        
-        if(real1>0 and real2>0):
-            if(abs(imag1)<1e-16):
-                fixed_point_types[i][j] = source_colour_value;
+        if(real1>1e-10 or real2>1e-10):
+            fixed_point_types[i][j]=5;
+            if(abs(imag1)<1e-10):
+                fixed_point_types[j][i] = source_colour_value;
             else:
-                fixed_point_types[i][j] = unstable_spiral_colour_value;
+                fixed_point_types[j][i] = unstable_spiral_colour_value;
 
         # If fixed point is a hyperbolic saddle.
         elif((real1>0 and real2<0) or (real1<0 and real2>0)):
                 fixed_point_types[i][j] = saddle_colour_value;
 
         # If the fixed point is asymptotically stable.
-        elif(real1<0 and real2<0):
+        elif(real1<1e-10 and real2<1e-10):
             if(abs(imag1)<1e-16):
-                fixed_point_types[i][j] = sink_colour_value;
+                fixed_point_types[j][i] = sink_colour_value;
             else:
-                fixed_point_types[i][j] = stable_spiral_colour_value;
+                fixed_point_types[j][i] = stable_spiral_colour_value;
 
 
 # Plot types of fixed point in a bifurcatiion diagram.
 XX, YY = NP.meshgrid(a_values, b_values);
 fig = plt.figure();
 plt.title('Bifurcation Plot for No Delay Schnakenberg Kinetics');
-p = plt.imshow(fixed_point_types, extent=[0, max(a_values), max(b_values), 0]);
+p = plt.imshow(fixed_point_types, extent=[0, max(a_values), max(b_values), 0], cmap=plt.cm.get_cmap('jet'));
+plt.clim(0,10)
 #plt.colorbar(p);
 plt.xlabel('$a$');
+plt.xticks(fontsize=10)
+plt.yticks(fontsize=10)
+
 plt.ylabel('$b$');
 plt.gca().invert_yaxis()
 fig.savefig('fig2.png', dpi=300);
